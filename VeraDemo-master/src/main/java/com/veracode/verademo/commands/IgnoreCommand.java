9ff06edbd15064e9aa44b0b9af757b557bea094c
SilviAdinata <silvima@mtechpro.com>
2024-07-02 16:16:33 +0700

    test commit for today

--- VeraDemo-master/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
+++ VeraDemo-master/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
@@ -11,7 +11,7 @@ import org.apache.log4j.Logger;
 
 public class IgnoreCommand implements BlabberCommand {	
 	private static final Logger logger = LogManager.getLogger("VeraDemo:IgnoreCommand");
-	
+	//testing!!!!!!
 	private Connection connect;
 	
 	private String username;
